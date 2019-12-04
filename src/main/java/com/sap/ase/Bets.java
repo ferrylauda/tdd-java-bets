1e7ff2f91550295a9087bf3d0b9326d37a2c71b5
Sebastian Krieghoff <sebastian.krieghoff@sap.com>
2019-12-04 15:27:29 +0100

    Add Bets stub and ignore eclipse settings

---
+++ src/main/java/com/sap/ase/Bets.java
@@ -0,0 +1,5 @@
+package com.sap.ase;
+
+public class Bets {
+
+}
--- src/test/java/com/sap/ase/BetsTest.java
+++ src/test/java/com/sap/ase/BetsTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BetsTest {
-
+	
 	@Test
 	public void testName() throws Exception {
 		fail("Not yet implemented");
